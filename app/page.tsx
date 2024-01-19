76ce8a31e2537c747938660b599a98f6d91de3a7
0-Chan <kyc4421194@gmail.com>
2024-01-19 17:56:03 +0900

    Apply mp4 video file to homePage

--- app/page.tsx
+++ app/page.tsx
@@ -5,41 +5,46 @@ import Balancer from 'react-wrap-balancer';
 
 export default function Home() {
   return (
-    <Shell className="max-w-6xl">
-      <section
-        id="hero"
-        aria-labelledby="hero-heading"
-        className={cn(
-          'mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-4 py-12 text-center',
-          'md:pt-32',
-        )}
-      >
-        <div
+    <main>
+      <Shell className="max-w-6xl">
+        <section
+          id="hero"
+          aria-labelledby="hero-heading"
           className={cn(
-            'relative z-[-1] flex place-items-center after:absolute after:-z-20',
-            'before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[""] before:lg:h-[360px]',
-            'after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[""]',
+            'mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-4 py-12 text-center',
+            'md:pt-32',
           )}
         >
-          <Balancer
-            as="h1"
+          <div
             className={cn(
-              'relative text-3xl',
-              'sm:text-6xl md:text-6xl lg:text-7xl',
+              'relative z-[-1] flex place-items-center after:absolute after:-z-20',
+              'before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[""] before:lg:h-[360px]',
+              'after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[""]',
             )}
           >
-            한울드론
+            <Balancer
+              as="h1"
+              className={cn(
+                'relative text-3xl',
+                'sm:text-6xl md:text-6xl lg:text-7xl',
+              )}
+            >
+              한울드론
+            </Balancer>
+          </div>
+          <Balancer
+            className={cn(
+              'max-w-[42rem] leading-normal text-muted-foreground',
+              'sm:text-xl sm:leading-8',
+            )}
+          >
+            드론을 활용한 실내외 시설 점검과 실시간 관제 기술을 제공합니다.
           </Balancer>
-        </div>
-        <Balancer
-          className={cn(
-            'max-w-[42rem] leading-normal text-muted-foreground',
-            'sm:text-xl sm:leading-8',
-          )}
-        >
-          드론을 활용한 실내외 시설 점검과 실시간 관제 기술을 제공합니다.
-        </Balancer>
-      </section>
-    </Shell>
+          <video autoPlay loop muted className="">
+            <source src="/video/hero.mp4" type="video/mp4" />
+          </video>
+        </section>
+      </Shell>
+    </main>
   );
 }
