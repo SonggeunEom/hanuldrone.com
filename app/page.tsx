b98074910f6912374f47c159883909333649d5fe
0-Chan <kyc4421194@gmail.com>
2024-01-22 15:01:07 +0900

    Feat: Add FadeIn motion text while in the viewport
    Made with framer-motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import Balancer from 'react-wrap-balancer';
 import { Icons } from '@/components/icons';
 import { Shell } from '@/components/shell';
 
+import { fadeIn, staggerContainer } from '@/lib/motion';
 import { cn } from '@/lib/utils';
-import Balancer from 'react-wrap-balancer';
 
 export default function Home() {
   return (
@@ -35,17 +40,40 @@ export default function Home() {
           </div>
           <Balancer
             className={cn(
-              'max-w-[42rem] leading-normal text-muted-foreground',
+              'max-w-[52rem] leading-normal text-muted-foreground',
               'sm:text-xl sm:leading-8',
             )}
           >
-            비가시권 실내외 시설 점검, 실시간 관제 기술, 도서산간 배송 서비스 등
-            다양한 서비스를 제공합니다.
+            실내외 시설 점검, 실시간 관제 기술, 비가시권 도서산간 지역 물류
+            서비스 등 다양한 서비스를 제공합니다.
           </Balancer>
           <video autoPlay loop muted>
             <source src="/video/hero.mp4" type="video/mp4" />
           </video>
-          <Icons.chevronsDown className="h-8 w-8" />
+          <motion.div
+            animate={{ y: [0, 15, 0], opacity: 1, scale: 1 }}
+            transition={{ repeat: Infinity, duration: 2.5 }}
+          >
+            <Icons.chevronsDown className="h-8 w-8" />
+          </motion.div>
+          <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+            className="mx-auto flex-col"
+          >
+            <motion.p variants={fadeIn('up', 'tween', 0.2, 1)}>
+              <Balancer
+                className={cn(
+                  'max-w-[42rem] leading-normal text-muted-foreground',
+                  'sm:text-xl sm:leading-8',
+                )}
+              >
+                여러 성공적인 실증 사업 추진 경험을 바탕으로 인정 받고 있습니다.
+              </Balancer>
+            </motion.p>
+          </motion.div>
         </section>
       </Shell>
     </main>
