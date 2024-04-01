6b677e1d7792c521be665b93f5bbe071a8dd0919
0-Chan <kyc4421194@gmail.com>
2024-04-02 01:34:50 +0900

    style: 랜딩페이지 Timeline CSS 변경

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -15,7 +15,7 @@ interface TimelineProps {
 
 const Timeline: React.FC<TimelineProps> = ({ items }) => {
   return (
-    <ol className="relative left-8 top-8 border-l border-stone-200 backdrop-blur-sm">
+    <ol className="relative left-6 top-6 border-l border-stone-200 backdrop-blur-sm">
       {items.map((item, index) => {
         return <TimelineItem key={item.key ?? index} item={item} />;
       })}
@@ -27,7 +27,7 @@ const TimelineItem: React.FC<{ item: TimelineItem }> = ({ item }) => {
   const { dateTime, label, description } = item;
 
   return (
-    <li className={cn('mb-5 ml-3', 'md:mb-10 md:ml-6')}>
+    <li className={cn('mb-1.5 ml-3', 'md:mb-5 md:ml-6')}>
       <div>
         <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-stone-300"></div>
       </div>
@@ -36,7 +36,7 @@ const TimelineItem: React.FC<{ item: TimelineItem }> = ({ item }) => {
           <time
             className={cn(
               'mb-1 text-xs font-normal leading-none text-stone-600',
-              'sm:text-sm md:text-base lg:text-lg',
+              'sm:text-xs md:text-sm lg:text-base',
             )}
           >
             {dateTime}
@@ -54,7 +54,7 @@ const TimelineItem: React.FC<{ item: TimelineItem }> = ({ item }) => {
           <p
             className={cn(
               'mb-4 text-xs font-normal text-stone-500',
-              'sm:text-sm md:text-base lg:text-lg',
+              'sm:text-xs md:text-sm lg:text-base',
             )}
           >
             {description}
